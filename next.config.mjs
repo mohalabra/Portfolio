919da54fb347c98c59fa467b3054f5efb9749834
labrassi-mohamed <labrassi.mohamed@gmail.com>
2025-01-20 00:26:32 +0100

    Enhance SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'x-robots-tag',
+                value: 'index, follow', // Allow indexing
+              },
+            ],
+          },
+        ];
+      },
+};
 
 export default nextConfig;
