b9ae5137a3583e2ed1934cf76536fbfc280bdedc
Mohamed Labrassi <m.labrassi3458@uca.ac.ma>
2025-05-07 18:05:10 +0100

    Update experiences.data.ts

--- src/data/experiences.data.ts
+++ src/data/experiences.data.ts
@@ -1,30 +1,41 @@
-import { ExperienceType } from '../types/types';
+import { ExperienceType } from "../types/types";
 
 export const experiences: ExperienceType[] = [
-  // {
-  //   title: 'Full Stack developer ( 6 month )',
-  //   startDate: 'Febrary 2025',
-  //   endDate: 'June 2025',
-  //   where: 'AliDanTik IT Solusions Company Marrakech',
-  //   what: 'Final Year Project',
-  //   tools:
-  //     'Tools and Technologies (Backend: Nodejs | Frontend: Nextjs | DB: MongoDB, Redis)',
-  // },
   {
-    title: 'Full Stack developer ( 6 month )',
-    startDate: 'June 2023',
-    endDate: 'December 2023',
-    where: 'AliDanTik IT Solusions Company Marrakech',
-    what: 'Paid Internship',
+    title: "AI Engineer & Full Stack Developer",
+    startDate: "February 2025",
+    // endDate: 'December 2023',
+    where: "AliDanTik IT Solutions Company, Marrakech (Canadian Start-up)",
+    what: "Paid Internship",
     tools:
-      'Tools and Technologies (Backend: Nodejs | Frontend: Nextjs | DB: MongoDB, Redis)',
+      "AI: RAG System | Backend: Node.js | Frontend: Next.js | DB: MySQL / Redis / Qdrant (VectorDB)",
+    description: "",
   },
   {
-    title: 'Full Stack Developer (2 month)',
-    startDate: 'April 2022',
-    endDate: 'June 2022',
-    where: 'International innovation platform (IIP) Marrakesh',
-    what: 'Final Year Project',
-    tools: 'Tools and Technologies (Backend: Spring Boot | Frontend: Angular )',
+    title: "AI Researcher",
+    startDate: "February 2025",
+    // endDate: 'June 2025',
+    where: "Faculty of Science and Technologies, Marrakech",
+    what: "Master's Final Year Project",
+    tools: "Computer Vision / Transfer Learning / CNNs / LLMs",
+    description: "",
+  },
+  {
+    title: "Full Stack Developer (6 months)",
+    startDate: "June 2023",
+    endDate: "December 2023",
+    where: "AliDanTik IT Solutions Company, Marrakech (Canadian Start-up)",
+    what: "Paid Internship",
+    tools: "Backend: Node.js | Frontend: Next.js | DB: MongoDB / Redis",
+    description: "",
+  },
+  {
+    title: "Full Stack Developer (2 months)",
+    startDate: "April 2022",
+    endDate: "June 2022",
+    where: "International Innovation Platform (IIP), Marrakech",
+    what: "Bachelor's Final Year Project",
+    tools: "Backend: Spring Boot | Frontend: Angular | DB: MySQL",
+    description: "",
   },
 ];
